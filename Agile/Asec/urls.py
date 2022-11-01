086f188bc8aac85d1dee1f08082b46084d81a0eb
mourra950 <mourra24@gmail.com>
2022-11-01 23:04:57 +0200

    first pages
    initialized the first 2 pages in urls and in views to show login and registration

--- Agile/Asec/urls.py
+++ Agile/Asec/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+import views
 
 
 # our site url patterns
 urlpatterns=[
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
     
 ]
