e6f66e29df751333ba63a52b2cc4986c4526404a
MoatazEldeeb <moatazdx1@gmail.com>
2022-12-27 21:20:56 +0200

    Modified database to fix future bugs
    Co-Authored-By: aya ahmad gamal <71007442+Aya-gamal2211@users.noreply.github.com>
    Co-Authored-By: Shaimaa-moh <93445956+Shaimaa-moh@users.noreply.github.com>
    Co-Authored-By: Omar Mohamed <64339763+mourra950@users.noreply.github.com>
    Co-Authored-By: ranawalid <103863926+ranawalid@users.noreply.github.com>

--- Agile/Asec/models.py
+++ Agile/Asec/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
-    committeeId = models.ForeignKey("Committee", on_delete=models.CASCADE,blank=True,null=True)
-    roleId = models.ForeignKey("Role", on_delete=models.CASCADE,blank=True,null=True)
+    committeeId = models.ForeignKey("Committee", on_delete=models.CASCADE)
+    roleId = models.ForeignKey("Role", on_delete=models.CASCADE)
 
 
 class Role(models.Model):
-    name = models.CharField(max_length=20,blank=True,null=True)
+    name = models.CharField(max_length=20)
     urlId = models.ManyToManyField("Urls")
     # def __str__(self):
     #     return f"{self.id}: {self.name}"
 
 class Urls(models.Model):
-    name= models.CharField(max_length=20, default='null',unique=True)
+    name= models.CharField(max_length=20,unique=True)
     # def __str__(self):
     #     return f"{self.id}: {self.name}"
 
@@ -21,7 +21,7 @@ class Committee(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=255)
     photo = models.CharField(max_length=30)
-    headId = models.ForeignKey("User", on_delete=models.CASCADE,blank=True,null=True)
+    headId = models.ForeignKey("User", on_delete=models.CASCADE)
     # def __str__(self):
     #     return f"{self.id}: {self.name}"
    
